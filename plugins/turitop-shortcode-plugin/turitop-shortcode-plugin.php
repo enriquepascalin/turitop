6bdabc6ad654b73107251d455144d4370e3f4489
Enrique Pascalin <epascalin@autocondat.com>
2024-10-04 02:34:13 -0600

    delete plugins

--- plugins/turitop-shortcode-plugin/turitop-shortcode-plugin.php
+++ plugins/turitop-shortcode-plugin/turitop-shortcode-plugin.php
@@ -18,3 +18,43 @@
  * Requires at least: 6.5
  * Requires PHP: 7.4
  */
+
+
+/**
+ * Show simple shortcode
+ *
+ * @return string
+ */
+ function turitop_shortcode():string {
+    return "<p style='color:red;font-size: 36px;font-weight:bold;'>This is a static shortcode message!</p>";
+}
+add_shortcode("message", "turitop_shortcode"); //[message]
+
+/**
+ * Show Shortcode with parameters
+ *
+ * @param array $attributes
+ * @return string
+ */
+function turitop_handle_product_data(array $attributes): string {
+    $attributes = shortcode_atts(
+        ["name" => "Default name","tag"=> "Default tag"], 
+        $attributes,
+        'productx'
+    );
+
+    return "<h3 style='color:blue;'>Product Data:<br /> Name - {$attributes['name']}, Tag - {$attributes['tag']}</h3>";
+}
+add_shortcode("productx", "turitop_handle_product_data"); // [productx name="x" tag="y"]
+
+/**
+ * Show Shortcode from database
+ *
+ * @return string
+ */
+function turitop_show_post():string {
+    
+    
+    return "";
+}
+add_shortcode("show_post", "turitop_show_post");
